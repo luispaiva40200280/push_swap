e78367a5448966ff6689c3a02336258827011985
luispaiva40200280 <luispaiva8936@gmail.com>
2025-12-22 20:44:24 +0000

    1 file saved

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lpaiva <lpaiva@student.42porto.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/14 01:31:25 by lpaiva            #+#    #+#             */
-/*   Updated: 2025/12/22 20:13:52 by lpaiva           ###   ########.fr       */
+/*   Updated: 2025/12/22 20:44:08 by lpaiva           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,11 +95,11 @@ int	main(int argc, char **argv)
 	if (!list_a)
 		return (free(nbrs), ft_printf("Error\n") ,0);
 	handler_sort(list_a);
-	while (list_a)
+	/* while (list_a)
 	{
 		printf("[value: %d | index: %d] -> ", list_a->value, list_a->index);
 		list_a = list_a->next;
-	}
+	} */
 	printf("NULL\n");
 	return (free_list(list_a), free(nbrs), 0);
 }
